12a3a8298b0df35acd1711e979ff3cb71fc0c9ab
Kshitij Dubey <dubeyk@stpaulsschool.org.uk>
2026-05-11 20:40:35 +0100

    Polymorphism + HashSet Work second try

--- Inheritance/Student.cs
+++ Inheritance/Student.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Inheritance
+namespace Inheritance;
+
+
+public class Student : Person
 {
-    public class Student
+    int _yearGroup;
+    public Student(string fn, string ln, DateTime dob, int yearGroup)
+        : base(fn, ln, dob)
     {
-        
+        _yearGroup = yearGroup;
     }
+
+    public Student(string fn, string ln, DateTime dob)
+        : base(fn, ln, dob)
+    {
+        _yearGroup = (Age() < 5) ? 0 : Age() - 4;
+    }
+
+    public Student(string fn, string ln, string email, DateTime dob)
+        : base(fn, ln, email, dob)
+    {
+        _yearGroup = (Age() < 5) ? 0 : Age() - 4;
+
+    }
+
+    public new string UserName => base.UserName + "-" + _yearGroup.ToString();
 }
